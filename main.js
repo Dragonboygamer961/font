04d62c45ef5e423c36c86613838a13f4fa4c461b
Dragonboygamer961 <95893470+Dragonboygamer961@users.noreply.github.com>
2022-11-21 16:57:11 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var nose_X = 10;
+var nose_Y = 10;
+
+var leftWrist_X = 0;
+var rightWrist_X = 0;
+
+var length_side = 0;
+
+
+
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    background("coral");
+    canvas.position(500, 100);
+
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    my_poseNet = ml5.poseNet(video, modelLoaded);
+
+    my_poseNet.on('pose', gotPoses);
+
+}
+
+function draw() {
+    background("coral");
+    textSize(length_side);
+    stroke("maroon");
+    fill("gold");
+    //square(x,y,length)
+    text("money",nose_X,nose_Y);
+
+}
+
+function modelLoaded() {
+
+    console.log("your model is done");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        nose_X = results[0].pose.nose.x;
+        nose_Y = results[0].pose.nose.y;
+      
+        leftWrist_X = results[0].pose.leftWrist.x;
+        rightWrist_X = results[0].pose.rightWrist.x;
+
+        length_side = leftWrist_X-rightWrist_X;
+     
+    }
+  
+} 
+
